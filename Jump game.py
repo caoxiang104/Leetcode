de7da8f598f3ca9493ca5075f16fda0968969da7
caoxiang104 <caoxiang112@gmail.com>
2018-09-02 21:23:43 +0800

    跳跃游戏

--- Jump game.py
+++ Jump game.py
@@ -1,3 +1,11 @@
+# -*- coding:utf-8 -*-
+"""
+给定一个非负整数数组，你最初位于数组的第一个位置。
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+判断你是否能够到达最后一个位置。
+"""
+
+
 class Solution:
     def canJump(self, nums):
         """
@@ -13,13 +21,13 @@ class Solution:
                 return False
             if i + nums[i] >= len(nums) - 1:
                 return True
-            for j in range(i + 1, i + nums[i]):
+            for j in range(i + 1, i + nums[i] + 1):
                 if j + nums[j] >= len(nums) - 1:
                     return True
                 elif j + nums[j] > max_len:
                     max_len = j + nums[j]
                     start_point = j
-            i = start_point
+            i = max(i + 1, start_point + 1)
         return False
 
 
